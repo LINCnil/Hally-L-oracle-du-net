2cfb1dcc8ca750d9facff60e0985732bee4b31ef
Victoria Duchatelle <vicduchatelle@gmail.com>
2017-09-08 17:34:08 +0200

    FabricJs minified version
    for compatibility with Firefox

--- src/launchtwitter.js
+++ src/launchtwitter.js
@@ -10,12 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	chrome.tabs.insertCSS(null, { code: css_lastcouche });
 	chrome.tabs.insertCSS(null, { file: "/src/css/styletwitter.css" });
-	chrome.tabs.executeScript(null, { file: "/src/fabric.js" });
-	chrome.tabs.executeScript(null, { file: "/src/TweenLite.min.js" });
-	chrome.tabs.executeScript(null, { file: "/src/TimelineMax.min.js" });
+	chrome.tabs.executeScript(null, { file: "/src/fabric.min.js" });
 	chrome.tabs.executeScript(null, { file: "/src/jquery-3.2.1.min.js" });
 	chrome.tabs.executeScript(null, { file: "/src/jquery-ui.min.js" });
-	chrome.tabs.executeScript(null, { file: '/src/d3.min.js' });
 	document.getElementById("go").addEventListener("click",
 		function () {
 			window.close();
